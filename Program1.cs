ee063c39668f95c65bac71eeb981a8d62996af21
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-08-28 18:11:22 +0530

    uc1

---
+++ Program1.cs
@@ -0,0 +1,50 @@
+﻿namespace ExpectionDemo
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+
+                int[] a = new int[3];
+
+                a[0] = 1;
+                a[1] = 2;
+                a[2] = 34;
+
+                int c = 10, b = 10;
+                int d = c / b;
+                Exception.ReferenceEquals(a, d);
+                Console.WriteLine("LineExcueted");
+
+                for (int i = 0; i < a.Length; i++)
+                {
+                    Console.WriteLine(a[i]);
+                }
+            
+
+    }
+          catch (IndexOutOfRangeException e)
+            {
+                 console.writeline(e.Message);
+            }
+          catch(DivideByZeroException e)
+          {
+                 console.writeline(e.Message);
+          }
+
+        catch (Expection e)
+         {
+
+           }
+
+         finally
+      {
+        Console.WriteLine("finally exceuted");
+          }
+
+        }
+    }
+
+}
