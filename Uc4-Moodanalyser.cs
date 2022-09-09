70119184530e0e4e5b572d2986cab554ba79372b
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-09-09 15:35:42 +0530

    uc 4 Exception

---
+++ Uc4-Moodanalyser.cs
@@ -0,0 +1,33 @@
+﻿namespace Moodanalyzer
+{
+    public class MoodAnalyzer
+    {
+        private const string message = "I am in Sad Mood";
+        private string testmessage = "";
+        //public string Message
+        //{ 
+        //    get { return message; } 
+        //    set { message = value; }
+        //}
+        public MoodAnalyzer()
+        {
+
+        }
+        public MoodAnalyzer(string msg)
+        {
+            this.testmessage = msg;
+        }
+
+        public string AnalyseMood()
+        {
+            if (this.testmessage == message)
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "Happy";
+            }
+        }
+    }
+}
