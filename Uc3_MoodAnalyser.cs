43934248f43e0380e056a6e72c1a820ff26139de
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-09-09 15:33:06 +0530

    UC 2 Sadmood

---
+++ Uc3_MoodAnalyser.cs
@@ -0,0 +1,55 @@
+﻿
+namespace Moodanalyzer
+{
+    public class MoodAnalyzer
+    {
+        private const string message = "I am in Sad Mood";
+        private string testmessage = "";
+        //public string Message
+        //{ 
+        //    get { return message; } 
+        //    set { message = value; }
+        //}
+        public MoodAnalyzer()
+        {
+
+        }
+        public MoodAnalyzer(string msg)
+        {
+            this.testmessage = msg;
+        }
+
+        public string AnalyseMood()
+        {
+
+            try
+
+            {
+                if (this.testmessage == "NULL" || this.testmessage == string.Empty)
+                {
+                    throw new Exception("Mood is Null");
+                }
+
+                if (this.testmessage == message)
+                {
+                    return "SAD";
+                }
+                else if (this.testmessage == "I am in Happy Mood")
+                {
+                    return "Happy";
+                }
+                else
+                {
+                    throw new Exception("Mood is Null");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return "Happy";
+            }
+
+
+        }
+    }
+}
